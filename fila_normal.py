f82285f440c04394377993dc11a971de57e569b6
Anthony <tony.ueta@gmail.com>
2022-06-07 18:36:08 -0300

    adding tox.ini

--- fila_normal.py
+++ fila_normal.py
@@ -1,23 +1,22 @@
-from fila_base import FilaBase
+class FilaNormal:
+    codigo: int = 0 
+    fila = []
+    clintesatendidos = []
+    senhaatual: str = 
 
-
-class FilaNormal(FilaBase):
-    def gera_senha_atual(self) -> None:
-        self.senha_atual = f"NM{self.codigo}"
-
-    def reseta_fial(self) -> None:
+    def gerasenhaatual(self)->None:
+        self.senhaatual = f'NM{self.codigo}'
+        
+     def resetafila(self)->None:
         if self.codigo >= 100:
             self.codigo = 0
         else:
-            self.codigo = 1
-
-    def atualiza_fila(self) -> None:
-        self.reseta_fial()
-        self.gera_senha_atual()
-        self.fila.append(self.senha_atual)
-
-    def chama_cliente(self, caixa: int) -> str:
-        cliente_atual: str = self.fila.pop(0)
-        self.clientes_atendidos.append(cliente_atual)
-
-        return f"Cliente atual: {cliente_atual}, dirija-se ao caixa: {caixa}"
+            self.codigo += 1
+    def atualizafila(self)->None:
+        self.resetafila()
+        self.gerasenhaatual()
+        self.fila.append(self.senhaatual)
+     def chamacliente(self, caixa:int)->str:
+        clienteatual = self.fila.pop(0)
+        self.clintesatendidos.append(clienteatual)
+        return f'Cliente atual: {clienteatual}, dirija-se ao caixa {caixa}'
