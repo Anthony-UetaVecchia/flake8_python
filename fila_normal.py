9e876c6e2b5120964a88e8ecad3d72512ef3d295
Anthony <tony.ueta@gmail.com>
2022-06-07 22:32:27 -0300

    criando método template

--- fila_normal.py
+++ fila_normal.py
@@ -5,7 +5,7 @@ class FilaNormal(FilaBase):
     def gera_senha_atual(self) -> None:
         self.senha_atual = f'NM{self.codigo}'
 
-     def chama_cliente(self, caixa: int) -> str:
-        cliente_atual = self.fila.pop(0)
+    def chama_cliente(self, caixa: int) -> str:
+        cliente_atual: str = self.fila.pop(0)
         self.clientes_atendidos.append(cliente_atual)
         return f'Cliente atual: {cliente_atual}, dirija-se ao caixa {caixa}'
--- fila_prioritaria.py
+++ fila_prioritaria.py
@@ -5,18 +5,10 @@ class FilaPrioritaria(FilaBase):
     def gera_senha_atual(self) -> None:
         self.senha_atual = f'PR{self.codigo}'
 
-    def chama_cliente(self, caixa: int) -> None:
-        display = []
-		cliente_atual = self.fila.pop(0)
-		display.append(f'Cliente: ]{cliente_atual} - Caixa {caixa}')
-
-		if len(self.fila) >= 3:
-			display.append(f'Próximo: {self.fila[0]}')
-			display.append(f'Fique atento: {self.fila[1]}')
-
-		self.clientes_atendidos.append(cliente_atual)
-
-		return display
+    def chama_cliente(self, caixa: int) -> str:
+        cliente_atual = self.fila.pop(0)
+        self.clientes_atendidos.append(cliente_atual)
+        return f'Cliente: {cliente_atual}, dirija-se ao caixa: {caixa}'
 
     def estatistica(self, dia: str, agencia: str, flag: str) -> dict:
         if flag != 'detail':
